383793086e5af79ccbf4d8d01f1f740d5eba6f1e
Valeriia.Kurylko <reviliya@gmail.com>
2022-10-26 12:36:28 +0200

    dogsInfo section structure added

--- src/components/DogsInfo.js
+++ src/components/DogsInfo.js
@@ -1,1 +1,16 @@
+import react from "react"; 
+
+const DogsInfo = () => {
+    return 
+    <div>
+        <h2> Some info about breed </h2>
+        <Picture />
+        <div classname="dogsInfoBox">
+        <h3> Breed name </h3>
+        <h3> Origin </h3>
+        </div>
+        <Button> More info </Button>
+    </div>
+}
+
 export { DogsInfo };
