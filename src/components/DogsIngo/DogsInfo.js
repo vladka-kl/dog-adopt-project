5ee35b8b88fa4505fe4a8dd6ccf66ed425afaba7
Valeriia.Kurylko <reviliya@gmail.com>
2022-10-26 12:57:29 +0200

    added css modules for Footer and DogsInfo components

--- src/components/DogsIngo/DogsInfo.js
+++ src/components/DogsIngo/DogsInfo.js
@@ -1,10 +1,12 @@
 import react from "react"; 
+import { Dropdown } from "../Dropdown";
 import styles from "./dogsInfo.module.css"
 
 const DogsInfo = () => {
     return 
     <div>
         <h2> Some info about breed </h2>
+        <Dropdown />
         <Picture />
         <div classname={styles.infobox}>
         <h3> Breed name </h3>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./footer.modules.css"
+
+const Footer = () => {
+    <div className={styles.footer}>
+        <div className={styles.about}> About our project </div>
+        <div className={styles.contacts}></div>
+        <div className={styles.social}></div>
+    </div>
+}
+export { Footer };
