e4cb870bc6f1410cd77d02ff75a46240db77bc3b
Valeriia.Kurylko <reviliya@gmail.com>
2022-10-28 10:43:10 +0200

    bugs fixed

--- src/components/DogsInfo/DogsInfo.js
+++ src/components/DogsInfo/DogsInfo.js
@@ -10,7 +10,7 @@ import { Dropdown } from "./dropdown";
 
 const DogsInfo = () => {
     const [index, setIndex] = useState(0);
-    const [name, setName] = useState();
+    const [name, setName] = useState("");
 
     const handleClick = (event) => {
         setName(event.target.value);
@@ -18,21 +18,21 @@ const DogsInfo = () => {
     }
 
     const selectedDog = breedsData.find(singleBreed => singleBreed.name === name)
-
+    console.log(selectedDog)
     return <section className={styles.main}>
         <h2> Some info about breed </h2>
         <Dropdown onSelect={handleClick} />
         <div id="DogCart" className={styles.card}>
             <div id="dataContainer">
                 <h3> The best breed ever  </h3>
-                <BreedsInfoBox
+                {selectedDog && <BreedsInfoBox
                     key={index}
                     id={selectedDog.id}
                     name={selectedDog.name}
                     origin={selectedDog.origin}
                     life_span={selectedDog.life_span}
                     image={selectedDog.image.url}
-                />
+                />}
             </div>
         </div>
         < div className={styles.buttonPlace}>
