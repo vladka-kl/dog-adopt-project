3c26520d53c322fdf1efda2a6c9b08b946ba1f0f
VLADYSLAVA KLOVAK <vladaklovak@gmail.com>
2022-10-31 17:42:28 +0100

    no changes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,30 +1,17 @@
-import React from 'react';
-import { RandomDogSlider } from '../components/RandomDogSlider/RandomDogSlider'
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import { RandomDogSlider } from "../components/RandomDogSlider/RandomDogSlider";
+import "react-alice-carousel/lib/alice-carousel.css";
 import { AboutUsInfo } from "../components/AboutUsInfo/AboutUsInfo";
-import { RandomText } from "../components/AboutUsInfo/RandomText"
-import { DogsInfo } from '../components/DogsInfo/DogsInfo'
-
+import { RandomText } from "../components/AboutUsInfo/RandomText";
+import { DogsInfo } from "../components/DogsInfo/DogsInfo";
 
 function LandingPage() {
-<<<<<<< HEAD
-    return(
+  return (
     <div className="LandingPage">
-      <AboutUsInfo
-      title={RandomText.title}
-      text={RandomText.text}/>
-       <RandomDogSlider />
-    </div>)
-=======
-    return (
-        <div className="LandingPage">
-            <AboutUsInfo
-                title={RandomText.title}
-                text={RandomText.text} />
-            <RandomDogSlider />
-            <DogsInfo />
-
-        </div>)
->>>>>>> 9454a7054e755adf7d08c6fb2a2d3d4c0c07d51c
+      <AboutUsInfo title={RandomText.title} text={RandomText.text} />
+      <RandomDogSlider />
+      <DogsInfo />
+    </div>
+  );
 }
 export { LandingPage };
