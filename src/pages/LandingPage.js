9ccefc3c33abbbcfbb1f120d6046a6808e9e0031
VLADYSLAVA KLOVAK <vladaklovak@gmail.com>
2022-11-03 11:21:52 +0100

    landing page content fixed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,9 +6,12 @@ import { RandomText } from "../components/AboutUsInfo/RandomText";
 import { DogsInfo } from "../components/DogsInfo/DogsInfo";
 
 const LandingPage = () => (
-    <div className="LandingPage">
-        <h2>LandingPage</h2>
-    </div>
-)
+  <div className="LandingPage">
+    <h2>LandingPage</h2>
+    <AboutUsInfo />
+    <RandomDogSlider />
+    <DogsInfo />
+  </div>
+);
 
 export default LandingPage;
