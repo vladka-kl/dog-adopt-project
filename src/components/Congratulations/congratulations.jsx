4b0517f3c9d3a2fead8ccf4f1363b05a10ef3bf4
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-09 14:40:52 +0100

    congratulations structure improved

--- src/components/Congratulations/congratulations.jsx
+++ src/components/Congratulations/congratulations.jsx
@@ -1,14 +1,23 @@
-import styles from "./congrats.module.css"
+import styles from "./congrats.module.css";
+import { Link } from 'react-router-dom';
+import congratsPic from "./congratsPic.svg"
 
 const Congratulations = () => {
     return (
         <div classname={styles.congratsForm}>
-            <h1>
-                Hello
-            </h1>
-            <img></img>
-            <p> Congratulation </p>
-            <button> Click </button>
+            <h1> Hello! </h1>
+            <div className={styles.container}>
+                <div classname={styles.pictureElement}>
+                    <img src={congratsPic} alt="congratsPic"></img>
+                </div>
+                <div className={styles.textElement}></div>
+                <div className={styles.textBlock}>
+                    <p> Congratulation   text ff </p>
+                </div>
+            </div>
+            <Link to='/'>
+                <button className={styles.backToHomeBtn}> To home page </button>
+            </Link>
         </div>
     )
 }
