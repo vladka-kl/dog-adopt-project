026c29cc5de761e65266806556e416ed6a4e30fc
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-09 16:11:24 +0100

    +footer and header

--- src/components/Congratulations/congratulations.jsx
+++ src/components/Congratulations/congratulations.jsx
@@ -5,15 +5,15 @@ import congratsPic from "./congratsPic.svg"
 const Congratulations = () => {
     return (
         <div classname={styles.congratsForm}>
-            <h1> Hello! </h1>
+            <h1> Congratulations! </h1>
             <div className={styles.container}>
                 <div classname={styles.pictureElement}>
                     <img className={styles.pic} src={congratsPic} alt="congratsPic"></img>
                 </div>
                 <div className={styles.textElement}></div>
                 <div className={styles.textBlock}>
-                    <title> Your new friend is on the way home</title>
-                    <p> Congratulation text </p>
+                    <h3> Your new friend is on the way home</h3>
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
                 </div>
             </div>
             <Link to='/' className={styles.buttonPlace}>
--- src/pages/Congratulations.js
+++ src/pages/Congratulations.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Congratulations } from '../components/Congratulations/congratulations';
+import { Footer } from "../components/Footer/Footer";
+import Header from "../components/Header/index";
 
 const CongratulationsPage = () => (
   <div className='LandingPage'>
+    <Header />
     <Congratulations />
+    <Footer />
   </div>
 );
 
