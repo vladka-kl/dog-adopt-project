18812bbe847c37ab1997f4ad7d662e55dd790b1c
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-09 23:24:08 +0100

    dogs info section with API

--- src/components/DogsInfo/DogsInfo.js
+++ src/components/DogsInfo/DogsInfo.js
@@ -1,39 +1,61 @@
 import React, { useEffect, useState } from "react";
-// import { Dropdown } from "../Dropdown";
 // import { Button } from "../Button";
 // import {Picture } from ".Picture";
 import styles from "./dogsInfo.module.css"
-import { breedsData } from "./data";
+//import { breedsData } from "./data";
 import { BreedsInfoBox } from "./BreedsInfoBox";
 import { Dropdown } from "./dropdown";
 import Union1 from "./Union1.svg"
 import { Link } from 'react-router-dom';
+import { useFetch } from "./dogsFetch";
+
+
 
 const DogsInfo = () => {
-    const [index, setIndex] = useState(0);
-    const [name, setName] = useState("");
+    const [dogsInfoResp, errorResp, isLoading] = useFetch(
+        `https://api.thedogapi.com/v1/breeds`
+    );
+    const [selectedDogName, setSelectedDogName] = useState(null);
 
-    const handleClick = (event) => {
-        setName(event.target.value);
-        console.log(event.target.value);
+
+    if (isLoading) {
+        return <h2>still in process, loading...</h2>;
     }
 
-    const selectedDog = breedsData.find(singleBreed => singleBreed.name === name)
-    console.log(selectedDog)
+    if (errorResp) {
+        console.log("Error: ", errorResp);
+        return <h2>an error has occurred, please contact the support</h2>;
+    }
+    const dogsArray = dogsInfoResp.data;
+
+    //const [index, setIndex] = useState(0);
+    //const [name, setName] = useState("");
+    //const [loading, setLoading] = useState(true);
+
+    const handleSelect = (event) => {
+        setSelectedDogName(event.target.value)
+    };
+
+
+    const selectedDogData = dogsArray.find(dog => dog.name === selectedDogName)
+
+    console.log('selectedDogData', selectedDogData)
+
     return <section className={styles.main}>
         <h2 className={styles.title2}> Some info about breeds </h2>
-        <Dropdown onSelect={handleClick} />
+
+        <Dropdown onSelect={handleSelect} />
+
         <div id="DogCart" className={styles.card}>
             <div id="dataContainer">
                 <h3 className={styles.title1}> The best breed ever  </h3>
                 <div className={styles.colorSquare}> </div>
-                {selectedDog && <BreedsInfoBox
-                    key={index}
-                    id={selectedDog.id}
-                    name={selectedDog.name}
-                    origin={selectedDog.origin}
-                    life_span={selectedDog.life_span}
-                    image={selectedDog.image.url}
+                {selectedDogData && <BreedsInfoBox
+                    id={selectedDogData.id}
+                    name={selectedDogData.name}
+                    origin={selectedDogData.origin}
+                    life_span={selectedDogData.life_span}
+                    image={selectedDogData.image.url}
                 />}
             </div>
         </div>
--- src/components/DogsInfo/dropdown.jsx
+++ src/components/DogsInfo/dropdown.jsx
@@ -1,14 +1,36 @@
 import styles from "./dogsInfo.module.css"
-import { breedsData } from "./data";
+//import { breedsData } from "./data";
 import React from "react"
+import axios from "axios";
+import { useFetch, useState } from "./dogsFetch";
+import { DogsInfo } from "./DogsInfo";
+
+
+
 const Dropdown = props => {
+    const [dogsInfoResp, errorResp, isLoading] = useFetch(
+        `https://api.thedogapi.com/v1/breeds`
+    );
+
+
+    if (isLoading) {
+        return <h2>still in process, loading...</h2>;
+    }
+
+    if (errorResp) {
+        console.log("Error: ", errorResp);
+        return <h2>please contact the support</h2>;
+    }
+
+    const dogsArray = dogsInfoResp.data;
+
     return (
         <select onChange={props.onSelect}
             className={styles.dropdown} >
             <option>Choose a breed</option>
-            {breedsData.map((singleBreed, index) =>
-                <option> {singleBreed.name}</option>
-            )}
+            {dogsArray.map(({ id, name }, index) => (
+                <option key={id || index}>{name} </option>
+            ))}
         </select>
     )
 }
