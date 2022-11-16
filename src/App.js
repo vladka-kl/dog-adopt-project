c7c374bb05261055e7716eb9b12ce58b3b08bbb1
Ahmed Elbehary <ahmedelbehary0@gmail.com>
2022-11-16 15:37:42 +0100

    Auto stash before merge of "Test" and "origin/Test"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 // @ts-nocheck
-import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Congratulations from "./pages/Congratulations";
-import Details from "./pages/Details";
-import ContactUs from "./pages/ContactUs";
-<<<<<<< HEAD
+import React, { useState } from 'react';
+import AppRoutes from './components/Route/AppRoutes';
+import { useFetch } from './hooks/useFetch';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Congratulations from './pages/Congratulations';
+import Details from './pages/Details';
+import ContactUs from './pages/ContactUs';
+import { Footer } from './components/Footer/Footer';
 import Header from "./components/Header";
-import useFetch from "./hooks/useFetch";
-=======
-import {Footer} from "./components/Footer/Footer";
->>>>>>> 35d68b7b06989689df1d464454e85fabce5f1fa8
 
 function App() {
   const [selectedDog, setSelectedDog] = useState({});
@@ -18,7 +16,11 @@ function App() {
     `https://api.thedogapi.com/v1/breeds`
   );
 
-<<<<<<< HEAD
+      <Header
+        selectedDog={selectedDog}
+        setSelectedDog={setSelectedDog}
+        dogsInfoResp={dogsInfoResp}
+      />
   if (isLoading || !dogsInfoResp.length) {
     return <h2>still in process, loading...</h2>;
   }
@@ -27,38 +29,6 @@ function App() {
     console.log("Error: ", errorResp);
     return <h2>an error has occurred, please contact the support</h2>;
   }
-
-  return (
-    <div className="App">
-      <Header
-        selectedDog={selectedDog}
-        setSelectedDog={setSelectedDog}
-        dogsInfoResp={dogsInfoResp}
-      />
-=======
-  useEffect(() => {
-    axios
-      .get('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        // handle success
-        if (
-          response &&
-          response.data &&
-          response.data.status === 'success'
-        ) {
-          console.log(response.data.message);
-          setData(response.data.message);
-        } else {
-          setData();
-        }
-      })
-      .catch(error => {
-        // handle error
-        console.log(error);
-        setData();
-      });
-  }, []);
-
   return (
     <div className='App'>
 >>>>>>> 35d68b7b06989689df1d464454e85fabce5f1fa8
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const useFetch = () => {
-  return <div>useFetch</div>;
+const useFetch = url => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        // handle success
+        if (
+          response &&
+          response.data &&
+          response.data.status === 'success'
+        ) {
+          setData(response.data.message);
+        } else {
+          setData([]);
+        }
+      })
+      .catch(error => {
+        // handle error
+        setData([]);
+      });
+  }, []);
+
+  return [data];
 };
 
 export { useFetch };
