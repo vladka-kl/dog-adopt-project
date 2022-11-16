643c6938673cde04ba37183de2077756b39b4407
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-11-16 16:41:42 +0100

    breakpoints fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import styles from './footer.module.css'
-import React from 'react'
-import FooterDog from '../../assets/images/FooterDog.png'
-import FooterDog1 from '../../assets/images/FooterDog1.png'
-import { Link } from 'react-router-dom';
-
-const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            <div className={styles.container}>
-                <div className={styles.colorBlock}>
-                    <p className={styles.text}> Adopt a Dog today </p>
-                    <Link to='/contact'>
-                        <button className={styles.checkBtn}> Contact us </button>
-                    </Link>
-                </div>
-                <div className={styles.picBlock}>
-                    <img src={FooterDog} alt="footer-dog" className={styles.picture}></img>
-                </div>
-            </div>
-            <div className={styles.copyright}> © Wilders team 2022 </div>
-=======
 import styles from "./footer.module.css";
 import React from "react";
 import FooterDog from "../../assets/images/FooterDog.png";
@@ -31,7 +8,6 @@ const Footer = () => {
       <div className={styles.picBlock}>
         <div className={styles.colorBlock}>
           <p className={styles.text}> Adopt a Dog today </p>
->>>>>>> 96a01c6 (folders orginised)
         </div>
         <img src={FooterDog} alt="footer-dog" className={styles.picture}></img>
       </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,13 +4,12 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import { AboutUsInfo } from "../components/AboutUsInfo/AboutUsInfo";
 import { RandomText } from "../components/AboutUsInfo/RandomText";
 import { DogsInfo } from "../components/DogsInfo/DogsInfo";
-import Welcome from "../components/welcome/Welcome";
+import Welcome from "../components/Welcome/Welcome";
 import Steps from "../components/Steps/Steps";
 import { Footer } from "../components/Footer/Footer";
 
 const LandingPage = ({ selectedDog, setSelectedDog, dogsInfoResp }) => (
   <div className="LandingPage">
-    <Header />
     <Welcome />
     <AboutUsInfo title={RandomText.title} text={RandomText.text} />
     <Steps />
@@ -23,7 +22,6 @@ const LandingPage = ({ selectedDog, setSelectedDog, dogsInfoResp }) => (
       setSelectedDog={setSelectedDog}
       dogsInfoResp={dogsInfoResp}
     />
-    <Footer />
   </div>
 );
 
