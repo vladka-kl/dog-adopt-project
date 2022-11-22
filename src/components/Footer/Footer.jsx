57fe5bff87bc502d74ff361f314a2350191c1613
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-11-22 18:45:42 +0100

    works fine

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import FooterDog from "../../assets/images/FooterDog.png";
 import { FaGithubSquare } from "react-icons/fa";
 import { AiFillLinkedin, AiOutlineInstagram } from "react-icons/ai";
 import Module from "./Modal";
+import { HashLink } from "react-router-hash-link";
 
 const linkNames = ["About", "Contact", "Our Team"];
 const socialMedia = ["Github", "LinkedIn", "Instagram"];
@@ -14,60 +15,54 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.colorBlock}>
-          <div className={styles.footerLinksGen}>
-            <h3 classname={styles.titleLinks}>General</h3>
-            <a href="#aboutUs" className={styles.textLink}>
-              <p>About</p>
-            </a>
-            <Link to="/contact" className={styles.textLink}>
-              <p>Contact</p>
-            </Link>
-            <Module>
-              <p>Our Team</p>
-            </Module>
-          </div>
-          <div className={styles.footerLinksSocial}>
-            <h3>Social Media</h3>
-            <a
-              className={styles.iconPersonal}
-              href="https://github.com/vladka-kl/dog-adopt-project"
-              target="_blank"
-            >
-              <i>
-                <FaGithubSquare
-                  color="#fffbf7"
-                  size="24"
-                  classname={styles.iconPersonal}
-                />
-              </i>
-            </a>
-            <a
-              className={styles.iconPersonal}
-              href="https://www.linkedin.com/"
-              target="_blank"
-            >
-              <i>
-                <AiFillLinkedin
-                  color="#fffbf7"
-                  size="24"
-                  classname={styles.iconPersonal}
-                />
-              </i>
-            </a>
-            <a
-              className={styles.iconPersonal}
-              href="https://www.linkedin.com/"
-              target="_blank"
-            >
-              <i>
-                <AiOutlineInstagram
-                  color="#fffbf7"
-                  size="24"
-                  classname={styles.iconPersonal}
-                />
-              </i>
-            </a>
-          </div>
+          <HashLink to="/#aboutUs" className={styles.textLink}>
+            <p>About Us</p>
+          </HashLink>
+          <Link to="/contact" className={styles.textLink}>
+            <p>Contact</p>
+          </Link>
+          <Module>
+            <p>Our Team</p>
+          </Module>
+          <a
+            className={styles.iconPersonal}
+            href="https://github.com/vladka-kl/dog-adopt-project"
+            target="_blank"
+          >
+            <i>
+              <FaGithubSquare
+                color="#fffbf7"
+                size="24"
+                classname={styles.iconPersonal}
+              />
+            </i>
+          </a>
+          <a
+            className={styles.iconPersonal}
+            href="https://www.linkedin.com/"
+            target="_blank"
+          >
+            <i>
+              <AiFillLinkedin
+                color="#fffbf7"
+                size="24"
+                classname={styles.iconPersonal}
+              />
+            </i>
+          </a>
+          <a
+            className={styles.iconPersonal}
+            href="https://www.linkedin.com/"
+            target="_blank"
+          >
+            <i>
+              <AiOutlineInstagram
+                color="#fffbf7"
+                size="24"
+                classname={styles.iconPersonal}
+              />
+            </i>
+          </a>
         </div>
         <img src={FooterDog} alt="footer-dog" className={styles.picture}></img>
       </div>
