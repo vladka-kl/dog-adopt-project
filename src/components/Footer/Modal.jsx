d6b9e9dfdb8cd26f47dbc1b640404cac53bc6942
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-11-30 13:20:50 +0100

    issue with the teams pop up resolved

--- src/components/Footer/Modal.jsx
+++ src/components/Footer/Modal.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./footer.module.css";
+import useOnClickOutside from "../../hooks/useOutsideClick";
 import { Link } from "react-router-dom";
 import { AiOutlineCloseCircle, AiFillLinkedin } from "react-icons/ai";
 import { FaDog, FaGithubSquare } from "react-icons/fa";
@@ -28,64 +29,62 @@ const ourTeamContent = [
 ];
 
 export default function Modal() {
-  const [modal, setModal] = useState(false);
-  const toggleModal = () => {
-    setModal(!modal);
-  };
-
+  const ref = useRef();
+  const [isModalOpen, setModalOpen] = useState(false);
+  useOnClickOutside(ref, () => setModalOpen(false));
   return (
     <>
-      <Link onClick={toggleModal} className={styles.btnModal}>
-        Our Team
-      </Link>
-      {modal && (
-        <div className={modal ? styles.activeModal : styles.closeModal}>
-          <div className={styles.overlay}></div>
-          <div className={styles.modalContent}>
-            {ourTeamContent.map((person, i) => {
-              return (
-                <div className={styles.prCard}>
-                  <i>
-                    <FaDog size="24" className={styles.iconDog} />
-                  </i>
-                  <h3 key={i}>{person.name}</h3>
-                  <div className={styles.links}>
-                    <a
-                      className={styles.iconPersonal}
-                      href={person.linkLI}
-                      target="_blank"
-                    >
-                      <i>
-                        <AiFillLinkedin
-                          size="24"
-                          classname={styles.iconPersonal}
-                        />
-                      </i>
-                    </a>
-                    <a
-                      className={styles.iconPersonal}
-                      href={person.linkGH}
-                      target="_blank"
-                    >
-                      <i>
-                        <FaGithubSquare
-                          size="24"
-                          classname={styles.iconPersonal}
-                        />
-                      </i>
-                    </a>
-                  </div>
+      {isModalOpen ? (
+        <div ref={ref} className={styles.modalContent}>
+          {ourTeamContent.map((person, i) => {
+            return (
+              <div className={styles.prCard}>
+                <i>
+                  <FaDog size="24" className={styles.iconDog} />
+                </i>
+                <h3 key={i}>{person.name}</h3>
+                <div className={styles.links}>
+                  <a
+                    className={styles.iconPersonal}
+                    href={person.linkLI}
+                    target="_blank"
+                  >
+                    <i>
+                      <AiFillLinkedin
+                        size="24"
+                        classname={styles.iconPersonal}
+                      />
+                    </i>
+                  </a>
+                  <a
+                    className={styles.iconPersonal}
+                    href={person.linkGH}
+                    target="_blank"
+                  >
+                    <i>
+                      <FaGithubSquare
+                        size="24"
+                        classname={styles.iconPersonal}
+                      />
+                    </i>
+                  </a>
                 </div>
-              );
-            })}
-
-            <button onClick={toggleModal} className={styles.btnModal}>
-              <span classname={styles.iconClose}>
-                <AiOutlineCloseCircle />
-              </span>
-            </button>
-          </div>
+              </div>
+            );
+          })}
+          <button
+            onClick={() => setModalOpen(!isModalOpen)}
+            className={styles.btnModal}
+          >
+            <span classname={styles.iconClose}>
+              <AiOutlineCloseCircle />
+            </span>
+          </button>
         </div>
+      ) : (
+        <Link onClick={() => setModalOpen(true)} className={styles.btnModal}>
+          Our Team
+        </Link>
       )}
     </>
   );
